67112e81f1ef503c0d44cd368428c65ccdcf3a45
Andrey Martynyuk <andrey@martynyuk.email>
2017-05-29 01:44:43 +0300

    byte_t should be defined outside of Neat namespace as well as _sz literal

--- Neat/Types.h
+++ Neat/Types.h
@@ -1,14 +1,11 @@
 #pragma once
 #include <stdint.h>
 
-namespace Neat
+inline size_t operator""_sz(unsigned long long value)
 {
-	inline size_t operator""_sz(unsigned long long value)
-	{
-		return static_cast<size_t>(value);
-	}
+	return static_cast<size_t>(value);
+}
 
 #ifndef byte_t
-	using byte_t = uint8_t;
+using byte_t = uint8_t;
 #endif
-}
