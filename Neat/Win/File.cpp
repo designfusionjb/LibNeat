eb825b37574e4b7ec6f06077d80546aecd9e0097
Andrey Martynyuk <andrey@martynyuk.email>
2017-09-03 17:15:06 +0300

    FileMapping is implemented

--- Neat/Win/File.cpp
+++ Neat/Win/File.cpp
@@ -3,6 +3,81 @@
 
 namespace Neat::Win
 {
+	FileView::FileView(byte_t* base, size_t size)
+		: m_base(base)
+		, m_size(size)
+	{
+	}
+
+	FileView::FileView(FileView&& other)
+		: m_base(other.m_base)
+		, m_size(other.m_size)
+	{
+		other.m_base = nullptr;
+		other.m_size = 0;
+	}
+
+	FileView& FileView::operator=(FileView&& other)
+	{
+		if (this != &other)
+		{
+			Unmap();
+			m_base = other.m_base;
+			m_size = other.m_size;
+			other.m_base = nullptr;
+			other.m_size = 0;
+		}
+		return *this;
+	}
+
+	FileView::~FileView()
+	{
+		if (m_base)
+			::UnmapViewOfFile(m_base);
+	}
+
+	void FileView::Flush(size_t bytes) const
+	{	
+		auto success = ::FlushViewOfFile(m_base, bytes);
+		if (!success)
+			throw LastErrorException();
+	}
+
+	void FileView::Unmap()
+	{
+		auto success = ::UnmapViewOfFile(m_base);
+		if (!success)
+			throw LastErrorException();
+		m_base = nullptr;
+		m_size = 0;
+	}
+
+	FileMapping::FileMapping(Handle&& handle)
+		: m_handle(std::move(handle))
+	{
+	}
+
+	FileView FileMapping::MapView(
+		DWORD dwDesiredAccess,
+		ULONGLONG ullFileOffset,
+		SIZE_T dwNumberOfBytesToMap) const
+	{
+		ULARGE_INTEGER offset;
+		offset.QuadPart = ullFileOffset;
+
+		auto base = ::MapViewOfFile(
+			m_handle,
+			dwDesiredAccess,
+			offset.HighPart,
+			offset.LowPart,
+			dwNumberOfBytesToMap);
+
+		if (!base)
+			throw LastErrorException();
+
+		return { static_cast<byte_t*>(base), dwNumberOfBytesToMap };
+	}
+
 	File::File()
 	{
 	}
@@ -85,5 +160,28 @@ namespace Neat::Win
 
 		if (!success)
 			throw LastErrorException();
+	}
+
+	FileMapping File::CreateMapping(
+		DWORD flProtect,
+		ULONGLONG ullMaximumSize,
+		LPCWSTR lpName,
+		LPSECURITY_ATTRIBUTES lpSecurityAttributes) const
+	{
+		ULARGE_INTEGER size;
+		size.QuadPart = ullMaximumSize;
+
+		Handle handle = ::CreateFileMappingW(
+			m_handle,
+			lpSecurityAttributes,
+			flProtect,
+			size.HighPart,
+			size.LowPart,
+			lpName);
+
+		if (!handle)
+			throw LastErrorException();
+
+		return handle;
 	}
 }
--- Neat/Win/File.h
+++ Neat/Win/File.h
@@ -1,9 +1,66 @@
 #pragma once
+#include "Neat\Types.h"
 #include "Neat\Win\Handle.h"
 #include <Windows.h>
 
 namespace Neat::Win
 {
+	class FileView
+	{
+		FileView(byte_t* base, size_t size);
+		FileView(const FileView& other) = delete;
+		FileView& operator=(const FileView& other) = delete;
+
+	public:
+		FileView(FileView&& other);
+		FileView& operator=(FileView&& other);
+		~FileView();
+
+		void Flush(size_t bytes = 0) const;
+		void Unmap();
+
+		byte_t* GetBase();
+		const byte_t* GetBase() const;
+		size_t GetSize() const;
+
+	private:
+		byte_t* m_base = nullptr;
+		size_t m_size = 0;
+
+		friend class FileMapping;
+	};
+
+	inline byte_t* FileView::GetBase()
+	{
+		return m_base;
+	}
+
+	inline const byte_t* FileView::GetBase() const
+	{
+		return m_base;
+	}
+
+	inline size_t FileView::GetSize() const
+	{
+		return m_size;
+	}
+
+	class FileMapping
+	{
+		FileMapping(Handle&& handle);
+
+	public:
+		FileView MapView(
+			DWORD dwDesiredAccess,
+			ULONGLONG ullFileOffset,
+			SIZE_T dwNumberOfBytesToMap) const;
+
+	private:
+		Handle m_handle;
+
+		friend class File;
+	};
+
 	class File
 	{
 	public:
@@ -31,6 +88,12 @@ namespace Neat::Win
 			LPCVOID lpBuffer,
 			DWORD nBytes,
 			LPOVERLAPPED lpOverlapped = nullptr);
+
+		FileMapping CreateMapping(
+			DWORD flProtect,
+			ULONGLONG ullMaximumSize = 0,
+			LPCWSTR lpName = nullptr,
+			LPSECURITY_ATTRIBUTES lpSecurityAttributes = nullptr) const;
 
 	private:
 		Handle m_handle;
